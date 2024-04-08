f115634c1037b2aeb3457789f69caea66a087d36
lhy_giytee <3170106234@zju.edu.cn>
2024-04-08 20:58:58 +0800

    reveal the timing limitation

---
+++ src/cb_grp_example/src/single_executor.cpp
@@ -0,0 +1,202 @@
+#include <chrono>
+#include <memory>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/syscall.h>
+#include <mutex>
+#include <coroutine>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/executor.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "cospike/coroutine.hpp"
+#include "cospike/generator.hpp"
+#include "cospike/task.hpp"
+#include "cospike/task_void.hpp"
+#include "cospike/executor.hpp"
+#include "cospike/awaiter.hpp"
+#include "cospike/io_utils.hpp"
+#include "cospike/channel.hpp"
+
+using namespace std::chrono_literals;
+
+using std::placeholders::_1;
+
+#define gettid() syscall(__NR_gettid)
+
+#define USE_INTRA_PROCESS_COMMS true 
+
+#define DUMMY_LOAD_ITER	1000
+#define THREAD_SIZE     3
+
+timeval starting_time;
+
+int dummy_load_calib = 1;
+
+void dummy_load(int load_ms) {
+    int i, j;
+    for (j = 0; j < dummy_load_calib * load_ms; j++)
+        for (i = 0 ; i < DUMMY_LOAD_ITER; i++) 
+            __asm__ volatile ("nop");
+}
+
+void dummy_load_sleep(int load_ms) {
+    int i, j;
+    // Do sth.
+    for (j = 0; j < dummy_load_calib * load_ms; j++)
+        for (i = 0 ; i < DUMMY_LOAD_ITER; i++) 
+            __asm__ volatile ("nop");
+    // Wait for the machine
+    sleep(1);
+    // Do sth. Further
+    for (j = 0; j < dummy_load_calib * load_ms; j++)
+        for (i = 0 ; i < DUMMY_LOAD_ITER; i++) 
+            __asm__ volatile ("nop");
+}
+
+namespace cb_chain_demo
+{
+class StartNode : public rclcpp::Node
+{
+public:
+    StartNode(const std::string node_name, const std::string pub_topic, int exe_time, int period, bool end_flag) 
+        : Node(node_name, rclcpp::NodeOptions().use_intra_process_comms(USE_INTRA_PROCESS_COMMS)), count_(0), exe_time_(exe_time), period_(period), end_flag_(end_flag)
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::String>(pub_topic, 1);
+
+        if (period_ == 10000)
+            timer_ = this->create_wall_timer(10000ms, std::bind(&StartNode::timer_callback, this));
+        else
+            timer_ = this->create_wall_timer( 2000ms, std::bind(&StartNode::timer_callback, this));
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+private:
+    size_t count_;
+    int exe_time_;
+    int period_;
+    timeval ctime, ftime, create_timer, latency_time;
+    bool end_flag_;
+
+    void timer_callback()
+    {
+        std::string name = this->get_name();            
+        gettimeofday(&ctime, NULL);
+        dummy_load(exe_time_);
+        auto message = std_msgs::msg::String();
+        message.data = std::to_string(count_++);
+        gettimeofday(&ftime, NULL);
+
+        int duration_us = (ftime.tv_sec - starting_time.tv_sec) * 1000000 + (ftime.tv_usec - starting_time.tv_usec);
+        long tv_sec = duration_us / 1000000;
+        long tv_usec = duration_us - tv_sec * 1000000;
+        RCLCPP_INFO(this->get_logger(), "Timer call [PID: %ld] back at %ld (s) %ld (us)", gettid(), tv_sec, tv_usec);
+
+        publisher_->publish(message);
+    }
+};
+
+class IntermediateNode : public rclcpp::Node
+{
+public:
+    IntermediateNode(const std::string node_name, const std::string sub_topic, const std::string pub_topic, int exe_time, bool end_flag) 
+        : Node(node_name), count_(0), exe_time_(exe_time), end_flag_(end_flag)
+    {                        
+        subscription_ = this->create_subscription<std_msgs::msg::String>(sub_topic, 1, std::bind(&IntermediateNode::callback, this, _1));
+        if (pub_topic != "") publisher_ = this->create_publisher<std_msgs::msg::String>(pub_topic, 1);
+    }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+private:
+    size_t count_;
+    int exe_time_;
+    timeval ctime, ftime;
+    double latency;
+    bool end_flag_;
+
+    void callback(const std_msgs::msg::String::SharedPtr msg) {
+        
+        gettimeofday(&ftime, NULL);
+        std::string name = this->get_name();
+
+        // Dummy with sleep (IO)
+        dummy_load_sleep(exe_time_);
+
+        auto message = std_msgs::msg::String();
+        message.data = msg->data;
+
+        int duration_us = (ftime.tv_sec - starting_time.tv_sec) * 1000000 + (ftime.tv_usec - starting_time.tv_usec);
+        long tv_sec = duration_us / 1000000;
+        long tv_usec = duration_us - tv_sec * 1000000;
+        RCLCPP_INFO(this->get_logger(), "Intermediate call [PID: %ld] back at %ld (s) %ld (us)", gettid(), tv_sec, tv_usec);
+        if (publisher_) publisher_->publish(message);
+    }       
+};
+}   // namespace cb_group_demo
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "PID: %ld run in ROS2.", gettid());
+
+    // Warm-up: dummy_load_calib
+    while (1) {
+        timeval ctime, ftime;
+        int duration_us;
+        gettimeofday(&ctime, NULL);
+        dummy_load(100); // 100ms
+        gettimeofday(&ftime, NULL);
+        duration_us = (ftime.tv_sec - ctime.tv_sec) * 1000000 + (ftime.tv_usec - ctime.tv_usec);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "dummy_load_calib: %d (duration_us: %d ns)", dummy_load_calib, duration_us);
+        if (abs(duration_us - 100 * 1000) < 500) {
+            break;
+        }
+        dummy_load_calib = 100 * 1000 * dummy_load_calib / duration_us;
+        if (dummy_load_calib <= 0) dummy_load_calib = 1;
+    }
+
+    // Define graph: c1 -> [c2, c3, c4]
+    // std::make_shared --> return the ptr & allocate the memory space on heap
+    auto c1_t_cb_0 = std::make_shared<cb_chain_demo::StartNode>("Timer_callback", "c1", 100, 1000, false);
+    auto c1_r_cb_1 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback1", "c1", "", 100, true);
+    auto c1_r_cb_2 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback2", "c1", "", 100, true);
+    auto c1_r_cb_3 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback3", "c1", "", 100, true);
+    auto c1_r_cb_4 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback4", "c1", "", 100, true);
+    auto c1_r_cb_5 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback5", "c1", "", 100, true);
+    auto c1_r_cb_6 = std::make_shared<cb_chain_demo::IntermediateNode>("Regular_callback6", "c1", "", 100, true);
+
+    // Create executors
+    rclcpp::executors::SingleThreadedExecutor exec1;
+    
+    // Allocate callbacks to executors
+    exec1.add_node(c1_t_cb_0);
+    exec1.add_node(c1_r_cb_1);
+    exec1.add_node(c1_r_cb_2);
+    exec1.add_node(c1_r_cb_3);
+    exec1.add_node(c1_r_cb_4);
+    exec1.add_node(c1_r_cb_5);
+    exec1.add_node(c1_r_cb_6);
+
+    // Record Starting Time:
+    gettimeofday(&starting_time, NULL);
+
+    // Spin lock
+    exec1.spin();
+
+    // Remove Extra-node
+    exec1.remove_node(c1_t_cb_0);
+    exec1.remove_node(c1_r_cb_1);
+    exec1.remove_node(c1_r_cb_2);
+    exec1.remove_node(c1_r_cb_3);
+    exec1.remove_node(c1_r_cb_4);
+    exec1.remove_node(c1_r_cb_5);
+    exec1.remove_node(c1_r_cb_6);
+
+    // Shutdown
+    rclcpp::shutdown();
+    return 0;
+}
